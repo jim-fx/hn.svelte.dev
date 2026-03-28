6b28e3b5ad3b41977848727167697d9e77cd7028
Max Richter <max@max-richter.dev>
2026-03-28 02:57:15 +0100

    feat: log zstd path

--- src/lib/db/constants.ts
+++ src/lib/db/constants.ts
@@ -3,8 +3,17 @@ import { DB_DIR } from '$env/static/private';
 import { existsSync } from 'fs';
 import { mkdirSync } from 'node:fs';
 
+function existsSafe(){
+  try {
+    return existsSync(ZSTD_PATH) || existsSync(ZSTD_PATH+".so")
+  }catch {
+    //
+  }
+  return false;
+}
+
 const ZSTD_PATH = resolve(DB_DIR, 'zstd_vfs.so');
-const IS_COMPRESSED = existsSync(ZSTD_PATH) || existsSync(ZSTD_PATH+".so");
+const IS_COMPRESSED = existsSafe();
 const DATA_DIR = DB_DIR ?? resolve('./data');
 
 if (!existsSync(DATA_DIR)) mkdirSync(DATA_DIR, { recursive: true });
