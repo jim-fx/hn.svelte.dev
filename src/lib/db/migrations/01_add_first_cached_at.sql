6c499f0d94aafa23c558bc2447952f3dc8adb0af
Max Richter <max.richter@dynabase.de>
2026-03-29 20:17:56 +0200

    fix: backfill first_cached_at

--- src/lib/db/migrations/01_add_first_cached_at.sql
+++ src/lib/db/migrations/01_add_first_cached_at.sql
@@ -1,5 +1,7 @@
 ALTER TABLE items
    ADD COLUMN first_cached_at INTEGER;
+UPDATE items SET first_cached_at = cached_at WHERE first_cached_at = null;
 
 ALTER TABLE users
    ADD COLUMN first_cached_at INTEGER;
+UPDATE users SET first_cached_at = cached_at WHERE first_cached_at = null;
